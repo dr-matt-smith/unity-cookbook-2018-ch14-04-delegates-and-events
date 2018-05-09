d639987ccb246cbf7412ea9d65669f4e1d23d294
dr-matt-smith <dr_matt_smith@me.com>
2018-05-09 21:49:05 +0100

    simplified code

--- Assets/_Scripts/ColorManager.cs
+++ Assets/_Scripts/ColorManager.cs
@@ -3,26 +3,26 @@ using UnityEngine;
 public class ColorManager : MonoBehaviour 
 {
     private ColorModel colorModel;
-
-    public ColorModel GetColorModel()
-    {
-        return this.colorModel;
-    }
+    private ColorChangeListenerConsole colorChangeListenerConsole;
 
     void Awake()
     {
         colorModel = new ColorModel();
+        colorChangeListenerConsole = new ColorChangeListenerConsole();
     }
 
-	public void BUTTON_ACTION_make_green(){
+	public void BUTTON_ACTION_make_green()
+    {
 		colorModel.SetColor(Color.green);
 	}
 
-	public void BUTTON_ACTION_make_blue(){
+	public void BUTTON_ACTION_make_blue()
+    {
 		colorModel.SetColor(Color.blue);
 	}
 
-	public void BUTTON_ACTION_make_red(){
+    public void BUTTON_ACTION_make_red()
+    {
 		colorModel.SetColor(Color.red);
 	}
 }
